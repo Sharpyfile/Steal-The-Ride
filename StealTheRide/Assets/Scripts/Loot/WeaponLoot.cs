39becd0810fe2a395036b46f28f88a3514bb75dc
kamiljan199 <kamiljan199@gmail.com>
2019-12-08 00:41:05 +0100

    First version of Weapon changing system (ammo). Now you have primary weapon ("1" Button), which you can change. Revolver is secondary weapon.

---
+++ StealTheRide/Assets/Scripts/Loot/WeaponLoot.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponLoot : MonoBehaviour
+{
+    public int weaponIndex;
+    public GameObject pickupText;
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            pickupText.SetActive(true);
+            collider.gameObject.GetComponentInChildren<WeaponSwitching>().WeaponToPickUp = weaponIndex;
+            collider.gameObject.GetComponentInChildren<WeaponSwitching>().weaponObjectToPickUp = this;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            pickupText.SetActive(false);
+            collider.gameObject.GetComponentInChildren<WeaponSwitching>().WeaponToPickUp = -1;
+            collider.gameObject.GetComponentInChildren<WeaponSwitching>().weaponObjectToPickUp = null;
+        }
+    }
+
+    public void Destroy()
+    {
+        Destroy(gameObject);
+    }
+
+    //private void OnTriggerEnter2D(Collider2D collider)
+    //{
+
+
+    //    if (collider.gameObject.tag == "Player" )
+    //    {
+    //        collider.gameObject.GetComponentInChildren<WeaponSwitching>().FirstWeapon = weaponIndex;
+    //        AudioManager.instance.Play("PickupAmmo");
+    //        Destroy(gameObject);
+    //    }
+    //}
+
+    //private void OnTriggerExit2D(Collider2D collider)
+    //{
+    //    //if (collider.gameObject.tag == "Player")
+    //    //{
+    //    //    collider.gameObject.GetComponentInChildren<WeaponSwitching>().FirstWeapon = weaponIndex;
+    //    //    AudioManager.instance.Play("PickupAmmo");
+    //    //    Destroy(gameObject);
+    //    //}
+    //}
+}
--- StealTheRide/Assets/Scripts/Player/WeaponSwitching.cs
+++ StealTheRide/Assets/Scripts/Player/WeaponSwitching.cs
@@ -7,6 +7,20 @@ public class WeaponSwitching : MonoBehaviour
     public int selectedWeapon = 0;
     public WeaponFire weaponScript;
 
+    //public WeaponFire firstWeapon;
+    //public WeaponFire secondWeapon;
+    public int firstWeapon = 1;
+
+    public int secondWeapon = 0;
+
+    public WeaponLoot weaponObjectToPickUp;
+    public int weaponToPickUp = -1;
+    public int WeaponToPickUp
+    {
+        get { return weaponToPickUp; }
+        set { weaponToPickUp = value; }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,21 +28,47 @@ public class WeaponSwitching : MonoBehaviour
     }
 
     // Update is called once per frame
+    //void Update()
+    //{
+    //    int previousSelectedWeapon = selectedWeapon;
+
+    //    if (Input.GetKeyDown(KeyCode.Alpha1))
+    //        selectedWeapon = 0;
+
+    //    if (Input.GetKeyDown(KeyCode.Alpha2))
+    //        selectedWeapon = 1;
+
+    //    if (Input.GetKeyDown(KeyCode.Alpha3))
+    //        selectedWeapon = 2;
+
+    //    if (Input.GetKeyDown(KeyCode.Alpha4))
+    //        selectedWeapon = 3;
+
+    //    if (previousSelectedWeapon != selectedWeapon)
+    //        SelectWeapon();
+    //}
+
     void Update()
     {
         int previousSelectedWeapon = selectedWeapon;
 
+        if (weaponToPickUp != -1 && Input.GetKeyDown(KeyCode.E))
+        {
+            if (selectedWeapon == firstWeapon)
+            {
+                selectedWeapon = weaponToPickUp;
+            }
+            firstWeapon = weaponToPickUp;
+            weaponObjectToPickUp.Destroy();
+        }
+
+
+
         if (Input.GetKeyDown(KeyCode.Alpha1))
-            selectedWeapon = 0;
-        
+            selectedWeapon = firstWeapon;
+
         if (Input.GetKeyDown(KeyCode.Alpha2))
-            selectedWeapon = 1;
-
-        if (Input.GetKeyDown(KeyCode.Alpha3))
-            selectedWeapon = 2;
-
-        if (Input.GetKeyDown(KeyCode.Alpha4))
-            selectedWeapon = 3;
+            selectedWeapon = secondWeapon;
 
         if (previousSelectedWeapon != selectedWeapon)
             SelectWeapon();
