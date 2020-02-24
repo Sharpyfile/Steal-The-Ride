bdda5b5c25d839c57916fe2f6b01ec55ad5f798f
kamiljan199 <kamiljan199@gmail.com>
2020-02-24 22:52:22 +0100

    New R, when weapon is empty. New shotgun mechanic with 5 shots.

--- StealTheRide/Assets/Scripts/Weapons/RevolverFire.cs
+++ StealTheRide/Assets/Scripts/Weapons/RevolverFire.cs
@@ -72,11 +72,12 @@ public class RevolverFire : WeaponFire
 
         if (bulletsInMagazine == 0 && isR == false)
         {
-            Vector3 newPosition = firePoint.position + new Vector3(0.0f, 0.5f, 0.0f);
-            GameObject newR = GameObject.Instantiate(r, newPosition, Quaternion.identity);
+            r.SetActive(true);
+            //Vector3 newPosition = firePoint.position + new Vector3(0.0f, 0.5f, 0.0f);
+            //GameObject newR = GameObject.Instantiate(r, newPosition, Quaternion.identity);
             isR = true;
-            StartCoroutine(MyDestroy(0.3f, newR));
-            //Destroy(newR);
+            StartCoroutine(StopR(0.3f));
+            ////Destroy(newR);
         }
 
         if (isReloading && timestampReload <= Time.time)
