b5f5dca1d6a37ab45ec91ac184dbf3c9172b95bd
Bromonov <pawel.ryczkowski12@gmail.com>
2020-02-26 14:39:27 +0100

    Started working with health bar.

--- StealTheRide/Assets/Scripts/Enemy/EnemyStatistics.cs
+++ StealTheRide/Assets/Scripts/Enemy/EnemyStatistics.cs
@@ -10,9 +10,22 @@ public class EnemyStatistics : ALootable
 
     private Transform player;
 
+    private HealthBar healthBar;
+
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
+        healthBar = GetComponent<HealthBar>();
+
+        if (gameObject.tag == "Boss")
+        {
+            healthBar.SetMaxHealth(enemyHealth);
+            healthBar.gameObject.SetActive(true);
+        }
+        else if (gameObject.tag == "Enemy")
+        {
+            healthBar.gameObject.SetActive(false);
+        }
     }
 
     void Update()
@@ -28,23 +41,25 @@ public class EnemyStatistics : ALootable
         Destroy(psObject, ps.main.duration);
 
         enemyHealth -= bullet.damage;
+
+        if (gameObject.tag == "Boss")
+        {
+            healthBar.setHealth(enemyHealth);
+        }
         Debug.Log("You hit the enemy!");
         if (enemyHealth <= 0)
         {
-            if (gameObject.tag == "Enemy")
+            if (gameObject.tag == "Enemy" || gameObject.tag == "Boss")
             {
                 AudioManager.instance.Play("Death");
                 Debug.Log("You killed the enemy!");
             }
             Drop();
             Destroy(enemy);
-            
-           
-                
         }
         else
         {
-            if (gameObject.tag == "Enemy")
+            if (gameObject.tag == "Enemy" || gameObject.tag == "Boss")
             {
                 AudioManager.instance.Play("Pain");
             }
